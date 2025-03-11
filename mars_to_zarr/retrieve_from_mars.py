6b0052839df412bffaf37608600e57697faf4759
Kasper Hintz <kah@dmi.dk>
2025-03-11 10:13:53 +0100

    use ECMWFService

--- mars_to_zarr/retrieve_from_mars.py
+++ mars_to_zarr/retrieve_from_mars.py
@@ -1,27 +1,32 @@
 import argparse
 
 from loguru import logger
-from ecmwfapi import ECMWFDataServer
+from ecmwfapi import ECMWFService
 
+EUROPEAN_AREA = "74/-27/33/45"
 
 def logging_wrapper(msg):
     logger.info(msg)
 
 
 def retrieve_data(args: argparse.Namespace):
-    server = ECMWFDataServer(log=logging_wrapper)
 
-    server.retrieve({
-        "class": "ti",
-        "dataset": "tigge",
-        "date": "2025-03-01/to/2025-03-09",
-        "expver": "prod",
-        "grid": "0.5/0.5",
+    server = ECMWFService("mars", log=logging_wrapper)
+
+    area = EUROPEAN_AREA
+
+    server.execute(
+    {
+        "class": "od",
+        "stream": "oper",
+        "date" : "2025-03-08",
+        "time": "12:00:00",
+        "expver": "1",
+        "Area": area,
+        "Grid": "0.25/0.25",
+        "type": "fc",
         "levtype": "sfc",
-        "origin": "ecmf",
-        "param": "167",
-        "step": "0",
-        "time": "00:00:00",
-        "type": "cf",
-        "target": "output"
-    })
+        "param": "151/165/166/167/172",
+        "step": "0/6/12/18/24",
+    },
+    "ifs.grib")
